25fb729e4f2edb6bd1239429cd9148b79ad0e1ed
AnuradhaKumar <anuradha@meenamuthukumars-MacBook-Pro.local>
2023-02-09 16:03:50 +0400

    Initial commit

---
+++ src/Maing.java
@@ -0,0 +1,14 @@
+public class Maing {
+    public class Main {
+
+
+        int add(int x, int y) {
+            for (int i = 0; i <15 ; i ++) {
+                System.out.println(x+y);
+            }
+            return 0;
+        }
+        public static void main(String[] args) {System.out.println("Testing git");}
+    }
+
+}
